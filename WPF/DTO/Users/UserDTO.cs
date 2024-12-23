d077c9ec1e1fb2f8196fba1738e916f566094dbe
Kyran-Git <niklauscareer@gmail.com>
2024-12-23 10:19:18 +0800

    Added the role property within the User files inside the WPF folder

--- WPF/DTO/Users/UserDTO.cs
+++ WPF/DTO/Users/UserDTO.cs
@@ -10,5 +10,6 @@ namespace WPF.DTO.Users
     {
         public string UserName { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
     }
 }
